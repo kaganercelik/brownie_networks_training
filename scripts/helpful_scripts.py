cd40156b3043ed7a5dbf24631df32dde6c1d5b0c
Yusuf Kağan <94568439+fuujazz@users.noreply.github.com>
2021-11-22 20:35:13 +0300

    Update helpful_scripts.py

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -13,7 +13,7 @@ STARTING_PRICE = 200000000000
 def get_account():
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active in FORKED_LOCAL_ENVIRONMENT
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENT
     ):
         return accounts[0]
     else:
